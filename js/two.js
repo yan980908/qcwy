d2cc65bee89ef9e27ad8bbb6fda4476793ac6e55
杜春艳 <1760729504@qq.com>
2022-06-04 11:22:31 +0800

    '修改提交'

--- js/two.js
+++ js/two.js
@@ -40,6 +40,99 @@ regist.onclick = function () {
     clearInterval(time)
     time = setInterval(fn, 1000)
 }
+
+
 // 判断手机号
+var phone = document.getElementsByClassName('iPhone')[0];
+var reg = document.getElementById('reg');
+var pas1 = document.getElementById('pas1');
+var pas2 = document.getElementById('pas2');
+var message = document.getElementsByTagName('i')
+
+phone.onblur = function () {
+    var reg = /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/;
+    if (phone.value == '') {
+        // message[0].style.display = 'block';
+        message[0].innerHTML = '✖'
+        // alert('输入的手机号不能位空')
+        return flase;
+    }
+    if (!reg.test(phone.value)) {
+        // message[0].style.display = 'block';
+        message[0].innerHTML = '✖';
+        // alert('输入的手机号为11位')
+        return flase;
+
+    }
+    else {
+        // message[0].style.display = 'block';
+        message[0].innerHTML = '✔';
+        // alert('输入的手机号正确')
+
+        return true;
+    }
+
+}
+
+// 验证码
+reg.onblur = function () {
+    if (reg.value == '') {
+        message[1].innerHTML = '✖'
+
+        return flase;
+    }
+    if (reg.value != regist.value) {
+        message[1].innerHTML = '✖'
+
+        return flase;
+
+    }
+    else {
+        message[1].innerHTML = '✔'
+        return true;
+
+    }
+}
+pas1.onblur = function () {
+    var reg1 = /^\d{1,6}$/;
+    if (pas1.value == '') {
+        message[2].innerHTML = '✖'
+
+        return flase;
+    }
+    if (!reg.test(pas1.value)) {
+        message[2].innerHTML = '✖'
+
+        return flase;
+
+    }
+    else {
+        // alert('验证码正确')
+        message[2].innerHTML = '✔'
+        return true;
+
+    }
+
+}
+pas2.onblur = function () {
+    if (pas2.value == '') {
+        // alert('密码不能为空')
+        message[3].innerHTML = '✖'
+        return flase;
+    }
+    if (pas1.value != pas2.value) {
+        // alert('密码格式有误')
+        message[3].innerHTML = '✖'
+        return flase;
+
+    }
+    else {
+        // alert('验证码正确')
+        message[3].innerHTML = '✔'
+        return true;
+
+    }
+
+}
 
 
